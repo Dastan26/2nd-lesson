30dc554650ad1b0f58c1e70726b8c1bae9710eb9
Dastan Kadylbekov <dastan.962601@gmail.com>
2022-09-08 20:21:09 +0600

    add 1-6 tasks

---
+++ index.js
@@ -0,0 +1,57 @@
+// let age = 20;
+
+//  if ( age >= 14 && age <= 90) {
+//      console.log(true)
+//  } else console.log(false)
+
+
+// let age = 10;
+
+// if ( age >= 14 && age <= 90) {
+//     console.log(true)
+// } else console.log(false);
+
+// let logIn = prompt('Введите логин', '');
+// let AdminPassword = 'password';
+
+
+// if (logIn === 'admin') {
+//     enterPassword = prompt('Введите пароль', '') 
+// }    else if (logIn === null || logIn === '') {
+//         alert('Отменено')
+//     }   else if (logIn !== 'admin') 
+//             alert('Я вас не знаю');
+
+// if (enterPassword === AdminPassword) {
+//     alert('Добро пожаловать!')
+// } else if (enterPassword === null || enterPassword === '') {
+//     alert('Отменено')
+// } else alert('Пароль неверный')
+
+
+// let number = prompt('Your number', '');
+
+// if (isNaN(number) === true){
+//     alert('Variable is not a number')
+// } else alert('Variable is a valid number')
+
+
+let num1 = 9;
+let num2 = 5;
+
+// if (num1 > num2) {
+//     alert('num1 is a largest')
+// } else if (num1 < num2) {
+//     alert('num2 is a largest')
+// } else alert('num1 and num2 are equal');
+
+
+let num3 = 4;
+
+if (num1 > num2 && num1 > num3) {
+    alert('num1 is a largest')
+} else if (num2 > num3) {
+    alert('num2 is a largest')
+} else alert('num3 is a largest');
+
+
