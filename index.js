2d78e6d8c60aabb00e05a09a42d7a22ed67ca985
Dastan Kadylbekov <dastan.962601@gmail.com>
2022-09-08 20:55:21 +0600

    second part of homeTasks

--- index.js
+++ index.js
@@ -36,8 +36,8 @@
 // } else alert('Variable is a valid number')
 
 
-let num1 = 9;
-let num2 = 5;
+// let num1 = 9;
+// let num2 = 5;
 
 // if (num1 > num2) {
 //     alert('num1 is a largest')
@@ -46,12 +46,63 @@ let num2 = 5;
 // } else alert('num1 and num2 are equal');
 
 
-let num3 = 4;
+// let num3 = 4;
 
-if (num1 > num2 && num1 > num3) {
-    alert('num1 is a largest')
-} else if (num2 > num3) {
-    alert('num2 is a largest')
-} else alert('num3 is a largest');
+// if (num1 > num2 && num1 > num3) {
+//     alert('num1 is a largest')
+// } else if (num2 > num3) {
+//     alert('num2 is a largest')
+// } else alert('num3 is a largest');
+
+
+let num1 = 5;
+let num2 = 9;
+
+operationName = prompt('enter the operation');
+switch (operationName) {
+    case 'add':
+        alert(num1 + num2);
+        break;
+
+    case 'subtract':
+        alert(num1 - num2);
+        break;
+
+    case 'multiply':
+        alert(num1 * num2);
+        break;
+
+    case 'devide':
+        alert(num1 / num2);
+        break;
+
+    case 'modulus':
+        alert(num1 % num2);
+        break;
+
+    default:
+        alert('invalid operation');
+}
+
+
+let name = "Ilya";
+
+alert(`hello ${1}`);
+
+alert(`hello ${"name"}`);
+
+alert(`hello ${name}`);
+
+
+let userName;
+
+userName = prompt('Как вас зовут?');
+alert('Привет' + ' ' + userName);
+
+const number = 9;
+alert(number ** 2);
+
+
+
 
 
